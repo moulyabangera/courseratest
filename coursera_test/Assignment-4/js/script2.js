f4a0f1ac457b565cb50d7cc74dbdb6ce9db02e2c
Honnappa B R <moulyabangera@gmail.com>
2018-06-26 07:30:11 +0530

    Assignment 4

---
+++ coursera_test/Assignment-4/js/script2.js
@@ -0,0 +1,14 @@
+(function (window) {
+  var jGreeter = {};
+  jGreeter.names = ["Moulya", "John", "Jason", "Ben", "Frank", "Jam"];
+  var greeting = "Hello ";
+  jGreeter.sayHi = function () {
+    console.log(greeting + jGreeter.names[i]);
+  }
+
+  window.jGreeter = jGreeter;
+
+})(window);
+
+
+
